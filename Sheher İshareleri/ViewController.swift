7738a82f5295aaf6a9554a8d8dcb2e55e9f09224
Yusif Huseynov <hv.yusif@mail.ru>
2018-12-31 22:12:27 +0400

    Initial Commit

---
+++ Sheher İshareleri/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sheher İshareleri
+//
+//  Created by Yusif Huseynov on 12/31/18.
+//  Copyright © 2018 Yusif Huseynov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
